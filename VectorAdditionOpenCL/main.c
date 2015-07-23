2be37a61bc4d6244432a822cdd9393b11256757f
Can <canfirtina@gmail.com>
2015-07-24 01:20:09 +0300

    Initial Commit

---
+++ VectorAdditionOpenCL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VectorAdditionOpenCL
+//
+//  Created by Can Firtina on 24/07/15.
+//  Copyright (c) 2015 Can Firtina. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
